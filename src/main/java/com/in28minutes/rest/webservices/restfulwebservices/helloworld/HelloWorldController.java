163894503982de246822e5a151c80e7550a03fa8
Srinivas Rangu <rangusrinu@Srinivass-MacBook-Pro.local>
2020-07-01 21:11:42 -0500

    Updated controller

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -15,13 +15,13 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
-		return new HelloWorldBean("Hello World Bean Madhavi!");
+		return new HelloWorldBean("Hello Madhavi!");
 	}
 	
 	///hello-world/path-variable/in28minutes
 	@GetMapping(path = "/hello-world/path-variable/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		//throw new RuntimeException("Something went wrong");
-		return new HelloWorldBean(String.format("Hello World, %s", name));
+		return new HelloWorldBean(String.format("Hello Chicago, %s", name));
 	}
 }
