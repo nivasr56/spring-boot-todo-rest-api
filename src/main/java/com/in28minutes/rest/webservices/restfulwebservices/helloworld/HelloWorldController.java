5bc0803fdc767a27042a9227bbedaede258fb0e8
Srinivas Rangu <rangusrinu@Srinivass-MacBook-Pro.local>
2020-07-01 20:41:11 -0500

    Changed name to Madhavi

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -15,7 +15,7 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
-		return new HelloWorldBean("Hello World Bean!");
+		return new HelloWorldBean("Hello World Bean Madhavi!");
 	}
 	
 	///hello-world/path-variable/in28minutes
